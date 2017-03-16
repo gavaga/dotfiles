9c308c83a9e507cc88157067269e450230928a78
Gavin McDowell <gmcdowell@college.harvard.edu>
2017-03-16 04:23:45 -0400

    screenlayout config

---
+++ system/.screenlayout/extern_tall.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI2 --mode 1920x1080 --pos 400x0 --rotate left --output HDMI1 --off --output DP1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x1920 --rotate normal --output VIRTUAL1 --off
